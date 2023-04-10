b72486b57a527ea594c80a6c57c647e20ebde952
Bruno <brunoluislima19@gmail.com>
2023-04-09 21:32:33 -0300

    Model for list Items and associated migrations

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])       
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith ouviu falar de uma nova aplicação online interessante
         # lista de tarefas. Ela decide verificar sua homepage
@@ -37,27 +42,21 @@ class NewVisitorTest(unittest.TestCase):
         # Quando ela tecla entar, a página é atualizada, e agora a página lista
         # "1: Buy peacock feathers" como um item em uma lista de tarefas
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        time.sleep(2)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # Ainda continua havendo uma caixa de texto convidado-a a acrescentar outro
         # item. Ela insere "use peacock feathers to make a fly" (Usar penas de pavão
         # para fazer um fly - Edith é bem metódica)
         inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         # A página é  atualizada novamente e agora mostra os dois itens em sua lista
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_element_by_tag_name("tr")
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers tom make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        
         # Edith se pergunta se o site lembrará de usa lista. Então ela nota
         # que o site gerou um URL único para ela --há um pequeno
         # texto explicativo para isso.
